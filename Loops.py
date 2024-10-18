cb2464e9fe635ac340f7a98c1c146e337cf23610
gulu864 <suva.kanta4u@gmail.com>
2024-10-18 18:58:19 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,24 @@
+#For loop
+for i in range(0,10,1):
+  print(i)
+
+print("------------")
+for i in range(1,10,2):
+  print(i)
+
+print("------------")
+for i in range(10,0,-1):
+  print(i)
+
+print("------------")
+x="Hello world!"
+for j in x:
+  print(j)
+
+print("------------")
+x="Hello world!"
+for j in x:
+  if j == "e":
+    print("yes found")
+  else:
+    print("e not found donut")
